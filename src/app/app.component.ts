1f353f5d86c635b1d080ea678350655e5c79c86c
jcvillacob <107330476+jcvillacob@users.noreply.github.com>
2022-11-11 09:28:07 -0500

    Citas creadas van al padre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'citas';
+  listCitas: any[] = [];
+
+  agregarCita(Cita: any) {
+    this.listCitas.push(Cita);
+  }
 }
--- src/app/components/crear-citas/crear-citas.component.ts
+++ src/app/components/crear-citas/crear-citas.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-crear-citas',
   templateUrl: './crear-citas.component.html',
-  styleUrls: ['./crear-citas.component.css']
+  styleUrls: ['./crear-citas.component.css'],
 })
 export class CrearCitasComponent implements OnInit {
+  @Output() CitaCreada = new EventEmitter<any>();
+  nombre = '';
+  fecha = '';
+  hora = '';
+  sintomas = '';
+  formularioIncorrecto = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  agregarCita() {
+    if (
+      this.nombre == '' ||
+      this.fecha == '' ||
+      this.hora == '' ||
+      this.sintomas == ''
+    ) {
+      this.formularioIncorrecto = true;
+      return;
+    }
+    this.formularioIncorrecto = false;
+
+    //Creamos objeto para enviarselo al padre
+    const Cita = {
+      nombre: this.nombre,
+      fecha: this.fecha,
+      hora: this.hora,
+      sintomas: this.sintomas,
+    };
+    this.CitaCreada.emit(Cita);
+    this.resetCampos();
   }
 
+  resetCampos() {
+    this.nombre = '';
+    this.fecha = '';
+    this.hora = '';
+    this.sintomas = '';
+  }
 }
